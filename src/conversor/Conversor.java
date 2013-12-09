ada47644fe066dac23631692504e0764b68ee7dd
Chema Valera Reales <chemaclass@outlook.es>
2013-12-09 02:52:18 +0100

    Implementando binario

---
+++ src/conversor/Conversor.java
@@ -0,0 +1,15 @@
+package conversor;
+
+/**
+ *
+ * @author chemaclass
+ */
+public class Conversor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal.main(args);
+    }
+}
